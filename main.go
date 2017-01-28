41d1211686795a62d2725220e39520924ac6e56e
pedrocaseiro <pcaseiro96@gmail.com>
2017-01-28 01:08:27 +0000

    imports discordgo lib, implements Discord session creation and opening, shutdown with ctrl+c

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
   "fmt"
   "flag"
 
-  // "github.com/bwmarrin/discordgo"
+  "github.com/bwmarrin/discordgo"
 )
 
 var token string
+var buffer = make([][]byte, 0)
 
 // init will run before main
 
@@ -21,5 +22,23 @@ func main() {
     fmt.Println("No token provided. Please run: duskbot -t <bot token>")
     return
   }
+
+  // Create a new Discord session using the provided bot token
+  discordGo, err := discordgo.New(token)
+  if err != nil {
+    fmt.Println("Error creating Discord session: ", err)
+    return
+  }
+
+  err = discordGo.Open()
+  if err != nil {
+    fmt.Println("Error opening discord session: ", err)
+    return
+  }
+
+  fmt.Println("Bot is now running. Press CTRL-C to exit.")
+  discordGo.Close()
+  <-make(chan struct{})
+  return
 }
 
